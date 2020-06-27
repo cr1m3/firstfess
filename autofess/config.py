6694b5459cc0ec727c886b50ee6640e0ff004e16
Akmal <magchuz@pm.me>
2020-06-27 16:27:25 +0700

    config: Use .env based configuration

--- autofess/config.py
+++ autofess/config.py
@@ -1,8 +1,14 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Load .env
+env_path = Path('.') / 'config.env'
+load_dotenv(dotenv_path=env_path)
 
 class Config:
-    CONSUMER_KEY = os.environ.get("CONSUMER_KEY", "")
-    CONSUMER_SECRET = os.environ.get("CONSUMER_SECRET", "")
-    ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
-    ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET", "")
-    TRIGGER_WORD = os.environ.get("TRIGGER_WORD", "!trigger")
+    CONSUMER_KEY = os.getenv("CONSUMER_KEY")
+    CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
+    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+    ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+    TRIGGER_WORD = os.getenv("TRIGGER_WORD")
