9f0ab207793b4374b4b0882eff84440e85ce174f
Akmal <magchuz@pm.me>
2020-07-05 13:16:32 +0700

    database: Change database engine to MongoDB Atlas
    Really bad implementation

--- autofess/utils/database.py
+++ autofess/utils/database.py
@@ -1,25 +1,19 @@
-import plyvel
-import msgpack
+from autofess import config
+from pymongo import MongoClient
 
 
-def _encode(value):
-    return msgpack.packb(value, use_bin_type=True)
-
-
-def _decode(value):
-    return msgpack.unpackb(value, raw=False)
-
+mongodbClient = MongoClient(config.DATABASE_URL)
+db = mongodbClient.datafess
 
 class Datafess:
-    def __init__(self, db):
-        self._db = plyvel.DB("database/" + db, create_if_missing=True)
+    def __init__(self, collection):
+        self._db = db[collection]
 
     def put(self, key, value):
-        value = _encode(value)
-        return self._db.put(key.encode("utf-8"), value)
+        return self._db.insert_one({ "key": key, "value" : value})
 
     def get(self, key):
-        return self._db.get(key.encode("utf-8"))
+        return self._db.find_one({ "key": key })
 
     def delete(self, key):
-        return self._db.delete(key.encode("utf-8"))
+        return self._db.delete_one({ "key" : key })
