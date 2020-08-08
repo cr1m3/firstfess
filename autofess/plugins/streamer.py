8b47b92fca28f2825ddecb3c6954c540bfce4fc8
Akmal <magchuz@pm.me>
2020-08-08 15:36:17 +0700

    streamer: Remove unused variable

--- autofess/plugins/streamer.py
+++ autofess/plugins/streamer.py
@@ -16,6 +16,5 @@ class StreamEvent(twitivity.Event):
             sender_id = message["sender_id"]
             if int(sender_id) == me.id:
                 return
-            text = message["message_data"]["text"]
             reply_text = responders.process_fess(message)
             responders.api.send_direct_message(sender_id, spintax.spin(reply_text))
